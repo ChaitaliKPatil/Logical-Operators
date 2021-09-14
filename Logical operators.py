4ebe09b357775807289d69a111355c0757301d83
ChaitaliKPatil <90540795+ChaitaliKPatil@users.noreply.github.com>
2021-09-14 21:54:00 +0530

    Create Logical operators.py

---
+++ Logical operators.py
@@ -0,0 +1,13 @@
+#Q5
+print('Q5: Logical operators like and, or, not')
+a=True
+b=False
+print('1.AND LOGIC\na=',a,'\tb=',b,'\ta and b=',a and b)#True and false gives false always
+print('a=',a,'\ta and a=',a and a)
+print('b=',b,'\tb and b=',b and b)
+print('2.OR LOGIC\na=',a,'\tb=',b,'\ta or b=',a or b)#true or false always gives true
+print('a=',a,'\ta or a=',a or a)
+print('b=',b,'\tb or b=',b or b)
+print('3.NOT LOGIC\na=',a,'\tnot a=',not a)#not true is always false and viz.
+print('b=',b,'\tnot b=',not b)
+print('\n\n\n\n')
